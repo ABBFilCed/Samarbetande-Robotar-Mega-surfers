3e3c7bd58287d6c1616003ea7643e391622c2362
abbjoaeri18 <43879119+Joaeri18@users.noreply.github.com>
2020-11-05 16:00:59 +0100

    Update test-uart-arduino.ino

--- test-uart-arduino/test-uart-arduino.ino
+++ test-uart-arduino/test-uart-arduino.ino
@@ -2,17 +2,21 @@ String payload, readString;
 
 void setup() {
   Serial.begin(115200);
+  Serial.setTimeout(100);
 }
 
 void loop(){
   while(Serial.available()){
     delay(1);
+    String err = Serial.readString();
+    Serial.println(err);
+    /*
     char c = Serial.read();
     payload += c;
     if(payload.length() > 4){
       Serial.println(payload);
-      payload = "";
-    }
+      payload = "";*/
   }
 }
+
 
